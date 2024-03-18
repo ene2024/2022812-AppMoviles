a98adcfe79b3999f6af79a0b88364534e1195069
ELIZA MIRANDA <josale1304@gmail.com>
2024-03-18 17:47:48 -0600

    Ciclo de vida de un componente

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'MiApp2022812',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
---
+++ src/app/home/home.page.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  constructor() {
+    console.log("AppComponent: Constructor");
+  }
+
+  ngOnChanges(){
+    console.log("AppComponen: OnChanges");
+  }
+
+  ngOnInit(){
+    console.log("AppComponent: OnInit");
+  }
+
+  ngDoCheck(){
+    console.log("AppComponent: DoCheck");
+  }
+
+  ngAfterContentInit(){
+    console.log("AppComponent: AfterContentInit");
+  }
+
+  ngAfterContentChecked(){
+    console.log("AppComponent:AfterContentChecked");
+  }
+
+  ngAfterViewInit(){
+    console.log("AppComponent:AfterViewInit");
+  }
+
+  ngAfterViewChecked(){
+    console.log("AppComponent: AfterViewChecked");
+  }
+
+  ngOnDestroy(){
+    console.log("AppComponent: OnDestroy");
+  }
+
+}
